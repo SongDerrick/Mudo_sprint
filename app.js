cde8c12a8fc9244abd9009e8fff772ba75ed111f
sd990307@gmail.com <songdoyoon@songdoyun-ui-MacBook-Pro.local>
2022-08-31 16:28:02 +0900

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/user');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(userRoutes);
+
+app.listen(3000);
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router(); // will create different paths with router function 
+
+const questionsController = require('../controllers/questions'); 
+// requiring question.js controller which functions main business logic
+
+router.get('/questions', questionsController.getSendQuestions);
+// '/questions' get method defined via controller -> this method shows questions
+
+router.post('/questions', questionsController.postUserData);
+// '/questions' get method defined via controller -> this method shows questions
+module.exports = router;
